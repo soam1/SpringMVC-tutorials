db16088919a87932736061887523a5dc95f7ee88
soam1 <soamakash9@gmail.com>
2024-06-29 15:57:07 +0530

    sending data from controller to view using model

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -2,14 +2,26 @@ package controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
         System.out.println("this is index page");
+        model.addAttribute("name", "Akash Soam");
+
+        List<String> friends = new ArrayList<String>();
+        friends.add("bajaj");
+        friends.add("atul");
+        friends.add("pandi");
+        friends.add("bhuria");
+        model.addAttribute("f", friends);
         return "index";
     }
 
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -9,5 +9,19 @@
     <h1>Welcome to the Basic JSP Page!(HOME page)</h1>
     <H2>we came here from the controller</H2>
     <p>This is a simple JSP page.</p>
+    <!-- <% %> these are script tags in jsp, also called jsp scriptlets, used to include java statements and expressions in jsp -->
+    <%@ page import ="java.util.ArrayList"%>
+    <%@ page import ="java.util.List"%>
+    <%
+    String name = (String) request.getAttribute("name");
+    List<String> friends = (List<String>) request.getAttribute("f");
+    %>
+    <p> My name is <%=name%></p>
+
+    <h3>My friends are:</h3>
+    <ul>
+        <% for(String f: friends) { %>
+        <li><%=f%></li>
+        <% } %>
 </body>
 </html>
